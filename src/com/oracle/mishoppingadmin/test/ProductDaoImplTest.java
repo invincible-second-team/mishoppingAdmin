337f29cfe091d104766b58e864725de7f38a3809
rzh <2224947710@qq.com>
2019-03-22 19:30:46 +0800

    上传

--- src/com/oracle/mishoppingadmin/test/ProductDaoImplTest.java
+++ src/com/oracle/mishoppingadmin/test/ProductDaoImplTest.java
@@ -10,7 +10,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class ProductDaoImplTest {
-    ProductDao productDao = new ProductDaoImpl();
+    private ProductDao productDao = new ProductDaoImpl();
 
     @Test
     public void categoryList() throws SQLException {
